cfc3e487c0d8460b05fb80d8e8660124374baae3
sixtemesseven <andreaskellermann@gmx.ch>
2017-08-16 23:22:28 +0200

    Added HP663xA test Script

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # visa-instruments
-Simple to use interface for different measurment devices using pyvisa and ni-visa.
+Simple to use interface for different measurment devices using pyvisa and ni-visa over GPIB, USB and ...
+
+Use simplevisa.py by including it in python 3.6+ project or import the content as project into eclipse / pydev.
+
+(Partially) Supported instruments
+	- HP856x Series Spectrum Analyzer 
+		Tested:
+			Agilent 8562EC
+	- HP663xA Series System Power Supplies 
+		Tested:
+			HP6632A
+			HP6622A)
+	- Rohde & Schwarz SMTxx Series Signal Generators
+		Tested:
+			SMT03
+			SMIQ03
+	- HP / Agilent Switch Module
+		Tested:
+			HP3488 
+	- Rigol Multimeter (Compatible with Agilent and Fluke Protocol)
+		Tested:
+			Rigol DM3068 (USB)
+
+
+Scripts
+	- HP662xA Test 
+		Runs trough CC and CV range while logging and ploting 			Voltage and Current by the Rigol DM3068 Multimeter.
---
+++ Scripts/HP663x_Test.py
@@ -0,0 +1,59 @@
+'''
+Created on Jul 2, 2017
+
+@author: sixtimesseven
+'''
+
+from simplevisa import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+HP663xAddr = 3
+GPIBPort = 0
+DM3068Addr = "USB0::0x1AB1::0x0C94::DM3O140600028::INSTR"
+
+FileName = input("Input File Name for CSV Output File")
+mode = input("1 = VC Mode, 2 = CC Mode")
+settleTime = input("Settle time before taking measurment [ms]")
+startmeasurement = input("Input Start voltage [V]")
+stopmeasurement = input("Input Stop voltage [V]")
+stepSize = input("Step Size [V]")
+
+if __name__ == '__main__':
+    
+    HP336x = HPPow(GPIBPort, HP663xAddr)
+    DMM = DM3068(DM3068Addr)
+
+    t = np.arange(startmeasurement, stopmeasurement, stepSize)
+    N = len(t)
+    measurement = []
+    i = N
+    
+    for index in range(N):
+        print(t[N - i])
+        if mode is 1:
+            HP336x.setvoltage(t[i], enable=True)
+            time.sleep(settleTime)
+            measurement.append(float(DMM.measurevoltage())) 
+        if mode is 2:
+            HP336x.setCurrent(t[i], enable=True)
+            time.sleep(settleTime)
+            measurement.append(float(DMM.measurecurrent())) 
+        else:
+            print("Error: Mode not Specified")
+            pass
+
+        print(measurement) 
+        i = i-1
+        
+    FileName = FileName + ".csv"
+                
+    f = open(FileName, "w")
+    for i in range(len(t)):
+        f.write("{} {}\n".format(t[i], measurement[i]))
+    f.close()
+        
+    plt.plot(t, measurement)
+    plt.show()
+    
+    pass
