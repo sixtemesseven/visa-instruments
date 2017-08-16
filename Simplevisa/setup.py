1bcc5b0790ccd1d6dbf75db53348b7f563ede398
sixtimesseven <al_qaida@gmx.net>
2017-08-17 01:23:57 +0200

    rand

---
+++ Simplevisa/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='simplevisa', 
+	packages = ['simplevisa'],  
+	description = 'Simplified PYVISA interface for random selection of instruments and functions', 	
+	version='17.08.2017b',
+	author = 'Andreas Kellermann',
+	author_email = 'al_qaida@gmx.net',
+	url = 'https://github.com/sixtemesseven/visa-instruments',
+	download_url = 'https://github.com//sixtimesseven/archive/17.08.2017b.tar.gz',    
+	keywords = ['testing', 'logging', 'pyvisa', 'GPIB', 'Agilent'],
+	classifiers = [],                                    
+)
--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-	name='simplevisa',    	
-	version='17.08.2017a',                         
-	scripts=['simplevisa']                  
-)
