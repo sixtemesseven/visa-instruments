6b4fb60d0245629b703355551323c46a104ddc1a
sixtemesseven <andreaskellermann@gmx.ch>
2017-08-16 22:49:07 +0200

    Addid Rigol Device

--- example.py
+++ example.py
@@ -4,34 +4,41 @@ Created on 08.02.2017
 @author: ak
 '''
 
-import simplevisa
+from simplevisa import HP856x
+from simplevisa import HPPow
+from simplevisa import HP3488
+from time import sleep
 
-if __name__ == '__main__':
+
+if __name__ == '__main__': 
+        
+    '''
+    switch = HP3488(0, 9)
+    switch.write("ID?")
+    print(switch.read())
+    ''' 
     
+    '''
+    #Open Power Supplies
+    pow1 = HPPow(0, 3)
+    pow2 = HPPow(0, 2)
 
+    pow1.setCurrent(0.5)
+    pow1.setVoltage(3.4, enable=True)
+    
+    pow2.setVoltage(5.1, channel=1)
+    pow2.setVoltage(5.1, channel=2)
+    pow2.setCurrent(1, channel=1)
+    pow2.setCurrent(1, channel=2)
 
-    if __name__ == '__main__':
+    #Open Spectrum Analyzer
+    sa = HP856x(0, 18)
+    sa.setSpan(1000)
+    sa.setCenter(10000)
+    #Show Waveforms (accumulative)
+    sa.monitor(True)
+    '''
+
+    pass
+
         
-        gen = simplevisa.RSSMTx(0, 28)
-        gen.setRF(1000, 0)
-        gen.outOff(False)
-        
-        sa = simplevisa.HP856x(0, 18)
-        sa.setSpan(1)
-        sa.setCenter(1000)
-        sa.monitor()
-        
-        
-        
-        '''    
-        ##Switch Example
-        #HP3488A0 = rm.open_resource('GPIB0::09::INSTR')
-        
-        ##Power Supply Exampe
-        #HP6632A = rm.open_resource('GPIB0::5::INSTR')
-        #HP6632A.write('CSET 1')
-        #HP6632A.write('VSET 9')
-        #print(HP6632A.query('VOUT?'))
-        '''
-        
-        pass
--- simplevisa.py
+++ simplevisa.py
@@ -36,6 +36,7 @@ class HP856x(object):
         self.rm = visa.ResourceManager()       
         self.instance = self.rm.open_resource(str(self.visaID))
 
+
     def commandInstrument(self, command):
         ''' 
         Send a GPIB command to instrument 
@@ -43,13 +44,14 @@ class HP856x(object):
         '''
         code = self.instance.write(str(command))
         if '<StatusCode.success: 0>' not in str(code):
-            raise Exception("HP856x device did not respond correctly!")
+            raise Exception("Device did not respond correctly!")
+        
         
     def queryInstrument(self, request):
         ''' 
         Query GPIB Device
         '''
-        return(self.instance.query(request))
+        return(self.instance.query(str(request)))
         
         
     def setSpan(self, spanMHz):
@@ -59,12 +61,14 @@ class HP856x(object):
         self.setSpan = "SP " + str(spanMHz) + "MHZ"
         self.commandInstrument(self.setSpan)
         print(self.setSpan)
+       
         
     def setCenter(self, centerMHz):
         ''' 
         set center frequency of spectrum analyzer in (float) MHz 
         '''
         self.commandInstrument('CF' + str(centerMHz) + 'MHZ')
+     
         
     def setExternalRef(self, external=None):
         ''' 
@@ -78,7 +82,27 @@ class HP856x(object):
             self.commandInstrument('FREF INT')
         return(self.queryInstrument('FREF?'))
     
-        
+    def setRBW(self, RBW=None):
+        ''' 
+        Set resolution bandwith manual in MHz or to auto rbw (float) (default=Auto)
+        '''
+        if RBW is None:
+            self.commandInstrument('RB AUTO')
+        else:
+            self.commandInstrument('RB' + RBW + 'MHZ')
+            
+    def setTitle(self, text):
+        ''' 
+        Writes a title on top of the sa display and vga output
+        '''
+        self.commandInstrument('TITLE' + text)
+
+    def getMarker(self, text):
+        ''' 
+        Writes a title on top of the sa display and vga output
+        '''
+        self.commandInstrument('TITLE' + text)
+            
     def getMeasurmentList(self):
         ''' 
         returns 601 frequency measurments as (float) list 
@@ -89,6 +113,7 @@ class HP856x(object):
         for i in range(601):
             self.saMeasList[i] = float(self.saMeasList[i])
         return(self.saMeasList)
+    
     
     def showGraph(self):
         '''
@@ -105,16 +130,15 @@ class HP856x(object):
         matplotlib.pyplot.title('HP8562 SA Measurment')
         matplotlib.pyplot.grid()    
         matplotlib.pyplot.show()
+     
         
     def monitor(self, persistance=None):
         ''' 
-        Shows and overdraws measurments from the spectrum analyzer when it updates 
+        Shows and overdraws measurments from the spectrum analyzer as it updates 
         
-        persistance overlays graphs continiously when (True)
+        Keywords:
+        persistance = overlays graphs continiously when on (bool) (Default=OFF)
         '''
-        if persistance is None:
-            persistance = False
-            
         xAxis = []
         for i in range(601):
             xAxis.append(i)
@@ -127,18 +151,14 @@ class HP856x(object):
         matplotlib.pyplot.grid()  
  
         def animate(i):
-            if persistance == False:
+            if persistance is None:
                 matplotlib.pyplot.clf()
             self.list = self.getMeasurmentList() 
             matplotlib.pyplot.scatter(xAxis, self.list, s=1)
-            
             self.line = matplotlib.pyplot.plot(xAxis, self.list) 
                         
         self.ani = animation.FuncAnimation(self.fig, animate, interval=10)
         matplotlib.pyplot.show()
-    '''
-    Constructor
-    '''
 
 
 class RSSMTx(object):
@@ -172,7 +192,7 @@ class RSSMTx(object):
         ''' 
         Query GPIB Device
         '''
-        return(self.instance.query(request))  
+        return(self.instance.query(str(request)))  
     
     def outOff(self, state):
         '''
@@ -216,6 +236,12 @@ class HP3488(object):
         if '<StatusCode.success: 0>' not in str(code):
             raise Exception("RS HP3488 device did not respond correctly!")
         
+    def queryInstrument(self, request):
+        ''' 
+        Query GPIB Device
+        '''
+        return(self.instance.query(request))
+        
     def writeRelay(self, module, relay, state):
         ''' 
         Sets relay to open or closed
@@ -234,19 +260,24 @@ class HP3488(object):
     
 class HPPow(object):
     '''
-    classdocs
-    This class provides an easy interface to multiple HP system power supplies 
-    
-    TODO: Only tested and implemented with com. over GPIB port --not RS232
+    Control HP/Agilent Power supplies
+    Confirmed working:
+        - HP6632A
+        - HP6622A (specify channel)
     '''
+    
     def __init__(self, bus, addr):
         ''' 
         Initiate GPIB instance 
+        
+        Keyword Arguments:
+        bus -- gpib bus number
+        addr -- device Adress
         '''
         self.visaID = 'GPIB' + str(bus) +'::' + str(addr) + '::INSTR'
         self.rm = visa.ResourceManager()       
         self.instance = self.rm.open_resource(str(self.visaID))
-        self.commandInstrument('*RST; *CLS')
+        self.commandInstrument('*RST; *CLS; STATus:PRESet; *SRE 0; *ESE 0; RST; CLR')
 
     def commandInstrument(self, command):
         ''' 
@@ -255,21 +286,179 @@ class HPPow(object):
         '''
         code = self.instance.write(str(command))   
         if '<StatusCode.success: 0>' not in str(code):
-            raise Exception("RS HP3488 device did not respond correctly!")
+            raise Exception("Device did not respond correctly!")
         
-    def setVoltage(self, channel, voltage):
+    def queryInstrument(self, request):
+        ''' 
+        Query GPIB Device
+        '''
+        return(self.instance.query(str(request)))
+    
+        
+    def setVoltage(self, voltage, channel=None, enable=True):
         ''' 
         Set voltage (float)
+        
+        Keywords:
+        voltage -- Set Voltage (float)
+        channel -- Channel Number, do not use if dev. has one channel only (default:None)
+        enable -- enable output (bool) (default is ON / Last device state)
+        
+        Return: 
+        Voltage (float)
         '''
-        self.commandInstrument('VSET' + str(channel) + ',' + str(voltage))
+        #Check if instrument output is disabled and if so keep it disabled unsless user enables it
+        if self.queryInstrument('OUT') is 0 and enable is None:
+            enable = False;
+        if enable is False:
+            self.commandInstrument('OUT 0')
+        else:
+            self.commandInstrument('OUT 1')
+        #Set channel if instrument has more than one channel
+        if channel is None:
+            self.commandInstrument('VSET' + str(voltage))
+        else:
+            self.commandInstrument('VSET' + str(channel) + ',' + str(voltage))
+        #Return the current output
         return self.queryInstrument('VOUT?')
+    
         
-    def setCurrent(self, channel, current):
+    def setCurrent(self, current, channel=None, enable=None):
         ''' 
-        Set voltage (float)
-        Return voltage (float)
+        Sets the devices Current
+        
+        Keywords:
+        current (float)
+        channel -- Channel Number, do not use if dev. has one channel only (default:None)
+        enable -- enable output (bool) (default is ON / last device state)
+        
+        Return current (float)
+        
+        
+        Set channel only if instruments has more than one channel!!!
         '''
-        self.commandInstrument('CSET' + str(channel) + ',' + str(current))
-        return self.queryInstrument('COUT?')
+        #Check if instrument output is disabled and if so keep it disabled unsless user enables it
+        if self.queryInstrument('OUT') is 0 and enable is None:
+            enable = False;
+        if enable is False:
+            self.commandInstrument('OUT 0')
+        else:
+            self.commandInstrument('OUT 1')
+        #Set channel if instrument has more than one channel
+        if channel is None:
+            self.commandInstrument('ISET' + str(current))
+        else:
+            self.commandInstrument('ISET' + str(channel) + ',' + str(current))
+        #Return the current output
+        return self.queryInstrument('IOUT?')
+    
+    
+    def setOverCurrent(self, enable, channel=None):
+        ''' 
+        Sets overcurrent Protection
+        
+        Keywords:
+        enable = Overcurrent protection on/off bool
+        channel = channel number
+
+        Set channel only if instruments has more than one channel!!!
+        '''
+        if channel is None:
+            self.commandInstrument('OCP' + str(int(enable)))
+        else:
+            self.commandInstrument('ISET' + channel + ',' + int(enable))
+            
+    def setOverVoltate(self, voltage, channel=None):
+        ''' 
+        Sets overcurrent Protection
+        
+        Keywords:
+        voltage = Overvoltage (V)
+        channel = channel number (default=None for single channel instruments)
+
+        Set channel only if instruments has more than one channel!!!
+        '''
+        if channel is None:
+            self.commandInstrument('OVSET' + voltage)
+        else:
+            self.commandInstrument('OVSET' + channel + ',' + voltage)
+            
+    
+    
+    def setUP(self, current, voltage, channel=None, enable=None, ocp=False, ovp=0):
+        ''' 
+        Sets most important device parameters
+        
+        Keywords:
+        current (float)
+        voltage (float)
+        channel -- Channel Number, do not use if dev. has one channel only (default:None)
+        enable -- enable output (bool) (default is ON / last device state)
+        ocp --- Set overcurrent protection (bool) (default is OFF)
+        ovp --- Overvoltage protection (default=0V)
+
+        Set channel only if instruments has more than one channel!!!
+        '''
+        self.setCurrent(current, channel=channel, enable=enable)
+        self.setVoltage(voltage, channel=channel, enable=enable)
+        self.setOverCurrent(ocp, channel=channel)
+        self.setOverVoltate(ovp, channel=channel)
+        
+    
+class DM3068(object):
+    '''
+    Control Rigol Multimeter DM3068
+    Confirmed working:
+    '''
+    
+    def __init__(self, USB):
+        ''' 
+        Initiate GPIB instance
+        '''
+        self.rm = visa.ResourceManager()       
+        self.instance = self.rm.open_resource(str(USB))
+        self.timeout = 250000
+
+
+    def commandInstrument(self, command):
+        ''' 
+        Send a GPIB command to instrument 
+        Raises exception if the device is unreachable
+        '''
+        code = self.instance.write(str(command))
+        if '<StatusCode.success: 0>' not in str(code):
+            raise Exception("Device did not respond correctly!")
+        
+        
+    def queryInstrument(self, request):
+        ''' 
+        Query GPIB Device
+        '''
+        return(self.instance.query(str(request)))
+    
+    def checkInstrument(self):
+        '''
+        Return: 
+        Voltage (float)
+        '''
+        self.commandInstrument('*RST')
+        self.commandInstrument('*CLS')
+        self.commandInstrument('*WAIT')
+        print(self.queryInstrument('*IDN?'))
+        return
+    
+        
+    def measureVoltage(self):
+        '''       
+        Return: 
+        Voltage (float)
+        '''
+        self.commandInstrument('CMDSET AGILENT')
+        v = self.queryInstrument('MEASure:Voltage:DC?')
+        return v 
+
+
+    
+   
 
         
