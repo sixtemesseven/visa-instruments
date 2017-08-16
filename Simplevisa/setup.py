c0de22f6d7a04c20af811c0bb25bf2557b53c64b
sixtimesseven <al_qaida@gmx.net>
2017-08-17 01:27:23 +0200

    rad

--- Simplevisa/setup.py
+++ Simplevisa/setup.py
@@ -7,7 +7,7 @@ setup(
 	version='17.08.2017b',
 	author = 'Andreas Kellermann',
 	author_email = 'al_qaida@gmx.net',
-	url = 'https://github.com/sixtemesseven/visa-instruments',
+	url = 'https://github.com/sixtemesseven/visa-instruments/simplevisa',
 	download_url = 'https://github.com//sixtimesseven/archive/17.08.2017b.tar.gz',    
 	keywords = ['testing', 'logging', 'pyvisa', 'GPIB', 'Agilent'],
 	classifiers = [],                                    
