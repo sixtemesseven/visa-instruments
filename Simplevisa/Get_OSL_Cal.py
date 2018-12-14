49ac20229ebdbbe386cc0b92b17538be41d89e9d
sixtimesseven <asixtimesseven@gmail.com>
2018-12-14 11:53:01 +0100

    started to seperate k cal calculations and x measurments in two files

--- Simplevisa/Get_OSL_Cal.py
+++ Simplevisa/Get_OSL_Cal.py
@@ -3,12 +3,24 @@ Created on Thu Dec 13 23:09:20 2018
 
 @author: justRandom
 
+WORK IN PROGRESS 
+
+Goal is to implement OSL cal formula as described in: http://www.hpl.hp.com/hpjournal/94oct/oct94a12.pdf
+
 Short script to load OSL cal data into .txt files @\HP3577_HighZImpeadanceMeas
 """
 
 import numpy as np
 import simplevisa #https://github.com/sixtemesseven/visa-instruments
 
+
+'''
+Function to get impeadance value - Page 68 Equ. 4
+'''
+def X(R, A, R0):
+    VShunt = np.subtract(R, A)   
+    I = np.divide(VShunt, R0)
+    return np.divide(A, I)
 
     
 vna = simplevisa.HP3577(0,11)
@@ -17,23 +29,44 @@ vna = simplevisa.HP3577(0,11)
 print("THIS WILL OVERWRITE YOUR EXISTING OSL CAL VALUES AT \HP3577_HighZImpeadanceMeas")
 print("Press any Key to start with OPEN measurment")
 input()
-R = vna.getDataNP('R')
-A = vna.getDataNP('A')
-np.savetxt('HP3577_HighZImpeadanceMeas\R_Open.txt', R.view(float).reshape(-1, 2)) #Save array to file
-np.savetxt('HP3577_HighZImpeadanceMeas\A_Open.txt', A.view(float).reshape(-1, 2)) #Save array to file
+R_Open = vna.getDataNP('R')
+A_Open = vna.getDataNP('A')
+
 
 print("Press any Key to start with SHORT measurment")
 input()
-R = vna.getDataNP('R')
-A = vna.getDataNP('A')
-np.savetxt('HP3577_HighZImpeadanceMeas\R_Short.txt', R.view(float).reshape(-1, 2)) #Save array to file
-np.savetxt('HP3577_HighZImpeadanceMeas\A_Short.txt', A.view(float).reshape(-1, 2)) #Save array to file
+R_Short = vna.getDataNP('R')
+A_Short = vna.getDataNP('A')
 
 print("Press any Key to start with 50OHM_LOAD  measurment")
 input()
-R = vna.getDataNP('R')
-A = vna.getDataNP('A')
-np.savetxt('HP3577_HighZImpeadanceMeas\R_50.txt', R.view(float).reshape(-1, 2)) #Save array to file
-np.savetxt('HP3577_HighZImpeadanceMeas\A_50.txt', A.view(float).reshape(-1, 2)) #Save array to file
+R_Load = vna.getDataNP('R')
+A_Load = vna.getDataNP('A')
+
+
+'''
+Parameters used for K calculation - Page 70 Equ. 5
+'''
+arr1 = np.full(400, 1) #Array filled with 1
+arrNeg1 = np.full(400, 1) #Array filled with -1
+R0 = np.full(400, 10.0 + 0j) #TODO get real measurments of shunt resistor (Non Ideal resistor so =/= 10+0j of course)
+Zlmi = X(R_Load, A_Load, R0) #Measured impeadance for load standard
+Yom = np.reciprocal(X(R_Open, A_Open, R0)) #Measured admitance for open standard
+Zsm = X(R_Short, A_Short, R0) #Measured impeadance for open standard
+A = np.divide(np.subtract(arr1, np.multiply(Zlmi, Yom)), np.subtract(Zlmi, Zsm))
+Zlsi = np.full(400, 49.2+0j) #TODO Zlsi should be the TRUE impeadance for the load standart (Non Ideal resistor so =/= 50+0j of course)
+
+
+'''
+Calculate K Parameters - Page 70 Equ. 5
+'''
+K1 = np.multiply(np.multiply(A, Zlsi), R0)
+K2 = np.divide(np.multiply(arrNeg1, Zsm), R0)
+K3 = np.multiply(np.multiply(arrNeg1, Yom), R0)
+
+np.savetxt('HP3577_HighZImpeadanceMeas\K1.txt', K1.view(float).reshape(-1, 2)) #Save array to file
+np.savetxt('HP3577_HighZImpeadanceMeas\K2.txt', K2.view(float).reshape(-1, 2)) #Save array to file
+np.savetxt('HP3577_HighZImpeadanceMeas\K3.txt', K3.view(float).reshape(-1, 2)) #Save array to file
+np.savetxt('HP3577_HighZImpeadanceMeas\R0.txt', K3.view(float).reshape(-1, 2)) #Save array to file #TODO get the real measured value based from real measurments depending on range...
 
 print("FINISHED")
